d19ec47751062f81b14253aca132df9ec4e99795
Elaine-DWL <1120164345@qq.com>
2018-10-24 20:34:40 +0800

    011&136

--- leetcode_661_image smoother/661/main.cpp
+++ leetcode_661_image smoother/661/main.cpp
@@ -26,22 +26,24 @@ public:
         int m=M.size(), n=M[0].size();
         vector<vector<int>> cnt(m+2, vector<int>(n+2, 1));//用于计数的矩阵，四周都是0，中间都是1
 
-        for(int j=0; j<cnt.size(); j++){// 将四周变成0
-            cnt[0][j] = 0;
+        for(int j=0; j<m+2; j++){// 将最左和最右两边变成0
             cnt[j][0] = 0;
-            cnt[cnt.size()-1][j] = 0;
-            cnt[j][cnt.size()-1] = 0;
+            cnt[j][n+1] = 0;
+        }
+        for(int j=0; j<n+2; j++){// 将最上和最下两边变成0
+            cnt[0][j] = 0;
+            cnt[m+1][j] = 0;
         }
         vector<int> a(n+2, 0);// 将添加
-        for(int i=0; i<M.size(); i++){// 添加最左和最右的0
+        for(int i=0; i<m; i++){// 添加最左和最右的0
             M[i].push_back(0);
             M[i].insert(M[i].begin(), 0);
         }
         M.insert(M.begin(), a);//添加最上面的0
         M.push_back(a);//添加最下面的0
         vector<vector<int>> res(m, vector<int>(n));//用于存储结果的矩阵
-        for(int i=1; i<M.size()-1; i++){
-            for(int j=1; j<M[0].size()-1; j++){
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<n+1; j++){
                 int sum = M[i-1][j-1] + M[i-1][j] + M[i-1][j+1]
                             +M[i][j-1] + M[i][j+1]
                             +M[i+1][j-1] + M[i+1][j] + M[i+1][j+1];
